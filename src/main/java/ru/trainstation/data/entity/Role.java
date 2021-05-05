fde6956b33d9ede3e39b9643a857a0a83ae79894
Popov Ivan <popov1703.ivan@yandex.ru>
2021-05-05 20:13:31 +0300

    Commit

--- src/main/java/ru/trainstation/data/entity/Role.java
+++ src/main/java/ru/trainstation/data/entity/Role.java
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Data
@@ -17,9 +20,30 @@ public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long roleId;
+
+    @NotEmpty(message = "Название роли не может быть пустым!")
+    @Size(message = "Минимум 2 символа!", min = 2)
     private String name;
 
     @OneToMany(mappedBy = "role")
     @JsonIgnoreProperties("role")
     private List<Client> clientList;
+
+    public Role(String role) {
+        this.name = role;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Role role = (Role) o;
+        return Objects.equals(roleId, role.roleId) &&
+                Objects.equals(name, role.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(roleId, name);
+    }
 }
